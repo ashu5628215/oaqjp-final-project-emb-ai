06e98adc2c48fd8da92ffed8727a9de179dc12cb
ashu5628215 <mnnitashu5628215@gmail.com>
2024-09-06 10:21:10 -0400

    itask1

---
+++ emotion_detection.py
@@ -0,0 +1,17 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+
+    request = { "raw_document": { "text": text_to_analyse } }
+
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+    response = requests.post(url, json=request, headers=header)
+
+
+    if response.status_code == 200:
+        return response.text
+    else:
+        return None
