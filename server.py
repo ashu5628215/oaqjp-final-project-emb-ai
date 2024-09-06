fa9dd33648a539bf920c825ed4b8f0604f430b60
ashu5628215 <mnnitashu5628215@gmail.com>
2024-09-06 10:43:47 -0400

    itask

---
+++ server.py
@@ -0,0 +1,27 @@
+
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+@app.route("/emotionDetector")
+def sent_analyzer():
+
+    text_to_analyze = request.args.get('textToAnalyze')
+    
+    result = emotion_detector(text_to_analyze)
+    
+    if result is None:
+        return "Invalid input! Try again."
+    
+    return (f"For the given statement, the system response is 'anger': {result['anger']}, "
+            f"'disgust': {result['disgust']}, 'fear': {result['fear']}"
+            f"'joy': {result['joy']} and "
+            f"'sadness': {result['sadness']}. The dominant emotion"
+            f" is {result['dominant_emotion']}.")
+@app.route("/")
+def render_index_page():
+
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
